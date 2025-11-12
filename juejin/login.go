1cf01885325fdfce24e4d90da6b1a80ad88cf1dd
pkc918 <xiaopan2000918@gmail.com>
2025-11-12 20:52:18 +0800

    feat: update login functionality to click login button

--- juejin/login.go
+++ juejin/login.go
@@ -9,6 +9,7 @@ func (j *JueJin) Login() error {
 	bin, _ := launcher.LookPath()
 	u := launcher.New().Bin(bin).Set("headless").Delete("--headless").MustLaunch()
 	page := rod.New().ControlURL(u).MustConnect().MustPage("https://juejin.cn/")
-	page.MustWaitStable().MustScreenshot("a.png")
+	page.MustElementX(`//*[@id="juejin"]/div[1]/div/header/div/nav/ul/ul/li[3]/div/button`).MustClick()
+	page.MustWaitStable().MustScreenshot("login.png")
 	return nil
 }
